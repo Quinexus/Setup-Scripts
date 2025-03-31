cc39a47283644457df5f512eb9406ff24b6a1013
Jovan Xavier <j.xavier1076@gmail.com>
2025-03-31 04:04:13 +0100

    changed

--- CGDSPackages.R
+++ CGDSPackages.R
@@ -1,82 +1,87 @@
-# Define utility functions for package installation
-install_if_missing <- function(pkg) {
-  if (!require(pkg, character.only = TRUE)) {
-    install.packages(pkg)
-  }
-}
-
-bioc_install_if_missing <- function(pkg) {
-  if (!requireNamespace(pkg, quietly = TRUE)) {
-    BiocManager::install(pkg)
+# Define universal package installation function
+check_install <- function(pkg, source = "CRAN", url = NULL) {
+  if (!require(pkg, character.only = TRUE, quietly = TRUE)) {
+    if (source == "CRAN") {
+      install.packages(pkg)
+    } else if (source == "Bioconductor") {
+      if (!require("BiocManager", quietly = TRUE)) install.packages("BiocManager")
+      BiocManager::install(pkg)
+    } else if (source == "source" && !is.null(url)) {
+      install.packages(url, repos = NULL, type = "source")
+    } else if (source == "github" && !is.null(url)) {
+      if (!require("devtools", quietly = TRUE)) install.packages("devtools")
+      devtools::install_github(url)
+    }
   }
 }
 
 # Install base packages
-install_if_missing("tidyverse")       # For data handling and visualisation
-install_if_missing("ggpubr")          # Publication-ready plots
-install_if_missing("openxlsx")        # Excel file handling
-
-# Bioconductor setup
-if (!requireNamespace("BiocManager")) install.packages("BiocManager")
-
-# Devtools for GitHub package installations
-if (!require("devtools")) install.packages("devtools")
-library(devtools)
+check_install("tidyverse") # For data handling and visualisation
+check_install("ggpubr") # Publication-ready plots
+check_install("openxlsx")
 
 # RNA-Seq packages
-bioc_install_if_missing("Biobase")
-bioc_install_if_missing("maftools")
-bioc_install_if_missing("DESeq2")
-bioc_install_if_missing("EnhancedVolcano")
-bioc_install_if_missing("AnnotationDbi")
-bioc_install_if_missing("org.Hs.eg.db")
-bioc_install_if_missing("clusterProfiler")
-bioc_install_if_missing("fgsea")
-bioc_install_if_missing("qusage")
-install_if_missing("msigdbr")
-install_if_missing("data.table")
+check_install("Biobase", source = "Bioconductor")
+check_install("maftools", source = "Bioconductor")
+check_install("DESeq2", source = "Bioconductor")
+check_install("EnhancedVolcano", source = "Bioconductor")
+check_install("AnnotationDbi", source = "Bioconductor")
+check_install("org.Hs.eg.db", source = "Bioconductor")
+check_install("clusterProfiler", source = "Bioconductor")
+check_install("fgsea", source = "Bioconductor")
+check_install("qusage", source = "Bioconductor")
+check_install("msigdbr")
+check_install("data.table")
 
 # DNA Methylation packages
-bioc_install_if_missing("minfi")
-bioc_install_if_missing("IlluminaHumanMethylation450kanno.ilmn12.hg19")
-bioc_install_if_missing("IlluminaHumanMethylation450kmanifest")
-bioc_install_if_missing("missMethyl")
-bioc_install_if_missing("minfiData")
-bioc_install_if_missing("DMRcate")
-bioc_install_if_missing("methylationArrayAnalysis")
+check_install("minfi", source = "Bioconductor")
+check_install("IlluminaHumanMethylation450kanno.ilmn12.hg19", source = "Bioconductor")
+check_install("IlluminaHumanMethylation450kmanifest", source = "Bioconductor")
+check_install("missMethyl", source = "Bioconductor")
+check_install("minfiData", source = "Bioconductor")
+check_install("DMRcate", source = "Bioconductor")
+check_install("methylationArrayAnalysis", source = "Bioconductor")
 
 # Proteomics packages
-install_if_missing("data.table")
-bioc_install_if_missing("limma")
-bioc_install_if_missing("ComplexHeatmap")
-devtools::install_github("CutillasLab/protools2@*release")
+check_install("data.table")
+check_install("limma", source = "Bioconductor")
+check_install("ComplexHeatmap", source = "Bioconductor")
+check_install("protools2", source = "github", url = "CutillasLab/protools2@*release")
 
 # ChIP-Seq packages
-bioc_install_if_missing("ChIPseeker")
-bioc_install_if_missing("TxDb.Hsapiens.UCSC.hg19.knownGene")
-bioc_install_if_missing("clusterProfiler")
+check_install("ChIPseeker", source = "Bioconductor")
+check_install("TxDb.Hsapiens.UCSC.hg19.knownGene", source = "Bioconductor")
+check_install("clusterProfiler", source = "Bioconductor")
 
 # Tumor Evolution packages
 system("wget https://cran.r-project.org/src/contrib/Archive/NORMT3/NORMT3_1.0.4.tar.gz")
-install_if_missing()("NORMT3_1.0.4.tar.gz", repos = NULL, type = "source")
-
-devtools::install_github("genome/bmm")
-devtools::install_github("genome/sciClone")
+check_install("NORMT3", source = "source", url = "NORMT3_1.0.4.tar.gz")
+check_install("bmm", source = "github", url = "genome/bmm")
+check_install("sciClone", source = "github", url = "genome/sciClone")
 
 # Cancer Genomics using Single-cell DNA-seq
-install_if_missing("gplots")
-install_if_missing("Rcpp")
-bioc_install_if_missing("BSgenome.Hsapiens.UCSC.hg38")
-bioc_install_if_missing("AneuFinder")
+check_install("gplots")
+check_install("Rcpp")
+check_install("BSgenome.Hsapiens.UCSC.hg38", source = "Bioconductor")
+check_install("AneuFinder", source = "Bioconductor")
 
 # Tumour microenvironment
-devtools::install_github("dviraran/xCell")
-devtools::install_github("GfellerLab/EPIC", build_vignettes=TRUE)
-install_if_missing("curl")
-devtools::install_github("ebecht/MCPcounter", ref="master", subdir="Source")
-devtools::install_github("BioInforCore-BCI/Decosus")
-install_if_missing("remotes")
-remotes::install_github("omnideconv/immunedeconv")
-bioc_install_if_missing("GVSA")
-bioc_install_if_missing("GSEABase")
-bioc_install_if_missing("limma")
+check_install("xCell", source = "github", url = "dviraran/xCell")
+check_install("EPIC", source = "github", url = "GfellerLab/EPIC")
+check_install("curl")
+check_install("MCPcounter", source = "github", url = "ebecht/MCPcounter")
+check_install("Decosus", source = "github", url = "BioInforCore-BCI/Decosus")
+check_install("remotes")
+check_install("immunedeconv", source = "github", url = "omnideconv/immunedeconv")
+check_install("GVSA", source = "Bioconductor")
+check_install("GSEABase", source = "Bioconductor")
+
+# Survival analysis
+check_install("randomForest")
+check_install("pamr")
+check_install("glmnet")
+check_install("caret")
+check_install("survival")
+check_install("survminer")
+check_install("survivalROC")
+check_install("pheatmap")
